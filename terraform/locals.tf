ae31a111caae09e45552ae268cea207d9c1028b7
DevopsTest.May14 <DevopsTest.May14@arroyoconsulting.net>
2024-06-09 07:59:48 -0400

    Adding tags

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,4 +5,12 @@ locals {
   aks_name            = "aks-devsu-devops-demo"
   aks_version         = "1.28.9"
   node_resource_group = "rg-devsu-devops-demo-nodes"
+
+  tags = {
+    Name = "Devsu DevOps Demo"
+    Environment = "dev1"
+    Company = "Devsu"
+    Owner = "Elvis Manuel Jorge Nunez"
+    Owner-Email = "elvismjorgen@gmail.com"
+  }
 }
