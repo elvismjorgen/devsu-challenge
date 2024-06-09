1fc7fa7933e7c3efd153ffd3cae27d770212771b
DevopsTest.May14 <DevopsTest.May14@arroyoconsulting.net>
2024-06-09 07:50:45 -0400

    Adding terraform config files to deploy AKS cluster and required Azure Services

---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env                 = "dev1"
+  region              = "eastus"
+  resource_group_name = "rg-devsu-devops-demo"
+  aks_name            = "aks-devsu-devops-demo"
+  aks_version         = "1.28.9"
+  node_resource_group = "rg-devsu-devops-demo-nodes"
+}
