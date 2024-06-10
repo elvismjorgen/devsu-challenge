09d7b1861c4d540efeab1656c8644818452f0c05
DevopsTest.May14 <DevopsTest.May14@arroyoconsulting.net>
2024-06-10 09:34:10 -0400

    Removing backend due problem connection to azure using Credentials

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-devsu-backend-tf"
-    storage_account_name = "sadevsubackendtf"
-    container_name       = "devsu-tf-state-files"
-    key                  = "dev1.terraform.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "rg-devsu-backend-tf"
+#    storage_account_name = "sadevsubackendtf"
+#    container_name       = "devsu-tf-state-files"
+#    key                  = "dev1.terraform.tfstate"
+#  }
+#}
